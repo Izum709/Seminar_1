ce82c817dca390f012267d4ef8f1fa3ca3b6a737
Alexandr Vinogradov <izum709@.com>
2023-04-09 20:00:20 +0300

    Add Squaring

---
+++ Squaring/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число "); 
+int number = int.Parse (Console.ReadLine());
+int squaring = (number * number);
+Console.Write(number);
+Console.Write("*");
+Console.Write(number);
+Console.Write("=");
+Console.Write(squaring);
