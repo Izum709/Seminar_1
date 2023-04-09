bc7f16580f7402c1b1a6bbc04bd0e09aefa4a8d4
Alexandr Vinogradov <izum709@.com>
2023-04-09 22:39:44 +0300

    Add test squaring

---
+++ Squaring_test/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введи число для проверки");
+int squaringNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("введи проверяемое число число ");
+int number = int.Parse(Console.ReadLine());
+if (squaringNumber == number *number)
+{
+    Console.WriteLine("Тыж мщй програмист! Молодец!");
+}
+else 
+{
+    Console.WriteLine("Попробуй еще раз, лошара!");
+}
