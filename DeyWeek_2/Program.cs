901e8ce054213a032c53573dc7271285c4962765
Alexandr Vinogradov <izum709@.com>
2023-04-10 18:32:09 +0300

    Add DeyWeek_2 как я хотел

---
+++ DeyWeek_2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число от 1 до 7:");
+int day = int.Parse(Console.ReadLine());
+if(day ==1) Console.WriteLine( "Это понедельник");
+if(day ==2) Console.WriteLine( "Это вторник");
+if(day ==3) Console.WriteLine( "Это среда");
+if(day ==4) Console.WriteLine( "Это четверг");
+if(day ==5) Console.WriteLine( "Это пятница");
+if(day ==6) Console.WriteLine( "Это суббота");
+if(day ==7) Console.WriteLine( "Это воскресенье");
+
